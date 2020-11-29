26d18f285da8daf9ab11a14f08c7316f01a780c3
pswitala <p.switala@pslab.eu>
2020-11-29 12:22:04 +0100

    Updated def for input var

--- vars.tf
+++ vars.tf
@@ -3,27 +3,51 @@ variable "pscloud_company" {}
 variable "pscloud_purpouse" {}
 
 variable "pscloud_agents" {
-    default = {        
+    type = map(object({
+        name = string
+        activation_key  = string  
+    }))
+    default = {          
     }
 }
 
 variable "pscloud_tasks" {
+    type = map(object({
+        name                = string 
+        source_arn          = string
+        destanation_arn     = string
+        bytes_per_second    = number
+        verify_mode         = string
+    }))
     default = {        
     }
 }
 
 variable "pscloud_location_smb" {
-    default = {        
-    }
-}
-
-variable "pscloud_location_nfs" {
+    type = map(object({
+        hostname            = string
+        subdirectory        = string
+        user                = string
+        password            = string
+        datasync_agent_arn  = string 
+    }))
     default = {        
     }
 }
 
 variable "pscloud_location_s3" {
+    type = map(object({
+        s3_arn              = string
+        subdirectory        = string
+        iam_role_arn        = string
+    }))
     default = {        
     }
 }
+variable "pscloud_location_nfs" {
+    default = {        
+    }
+}
+
+
 
